5a5c630acc32772ae0c3ad7b32e8b4d486cd2293
Sreyas-A-S <sreyasas25@gmail.com>
2026-01-22 10:38:46 +0530

    Changes

--- resources/views/admin/mindfulness_counsellors/index.blade.php
+++ resources/views/admin/mindfulness_counsellors/index.blade.php
@@ -353,7 +353,7 @@
     <div class="modal-dialog modal-lg modal-dialog-centered" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Practitioner Details</h5>
+                <h5 class="modal-title">Mindfulness Counsellor Details</h5>
                 <button class="btn-close" type="button" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body" id="view-modal-content">
