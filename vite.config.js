6221f87f8ca97eab1eeeb970b75a3451018fbcd4
Vishnu VIjayan <66822219+vishnuvijayan10@users.noreply.github.com>
2026-02-05 18:36:22 +0530

    Practitioner Registration Form Design

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/css/practitioner-register.css',
+                'resources/js/app.js',
+                'resources/js/country-selector.js'
+            ],
             refresh: true,
         }),
         tailwindcss(),
