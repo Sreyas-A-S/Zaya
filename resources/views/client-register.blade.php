0b9afedd89ee4f3bd29abeb59322125075129dae
abhishekgopan888 <aslamm63@gmail.com>
2026-04-25 17:45:44 +0530

    feat: add client registration page with custom input styling and nationality selector

--- resources/views/client-register.blade.php
+++ resources/views/client-register.blade.php
@@ -858,14 +858,8 @@
                             </a>
 
                             <button type="submit" id="submit-btn" class="w-full sm:w-auto bg-[#FABC41] text-[#423131] py-4 px-12 rounded-full font-black text-lg transition-all hover:bg-[#E8AA32] hover:-translate-y-0.5 shadow-xl shadow-[#FABC41]/20">
-<<<<<<< HEAD
-                                <i class="ri-loader-4-line ri-spin btn-loader mr-2"></i>
-                                <span class="hidden md:inline">{{ __('Complete & Proceed to Payment') }}</span>
-                                <span class="md:hidden">{{ __('Submit') }}</span>
-=======
                                 <i class="ri-loader-4-line ri-spin btn-loader hidden mr-2"></i>
                                 {!! __('Complete & Proceed to Payment') !!}
->>>>>>> d32b6fa2a6a653d1dfb438602f8d0074aa078717
                             </button>
                         </div>
                     </div>
